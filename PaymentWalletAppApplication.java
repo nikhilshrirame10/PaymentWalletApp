b5494602cf8671e4694ffff4ca7a7861eb8680b7
nikhilshrirame10 <139323990+nikhilshrirame10@users.noreply.github.com>
2023-09-28 22:04:39 +0530

    Add files via upload

---
+++ PaymentWalletAppApplication.java
@@ -0,0 +1,13 @@
+package com.jsp.PaymentWalletApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentWalletAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentWalletAppApplication.class, args);
+	}
+
+}
